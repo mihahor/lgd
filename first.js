dd3fc43887b160c10eeedd63b6f0a8f59c27301a
Mikhail <152221637+mihahor@users.noreply.github.com>
2023-11-27 20:52:42 +0000

    1

---
+++ first.js
@@ -0,0 +1,9 @@
+// Prompt the user for their name
+var userName = prompt("What is your name?");
+
+// Greet the user
+if (userName) {
+  alert("Hello, " + userName + "! Nice to meet you.");
+} else {
+  alert("Hello there! Nice to meet you.");
+}
